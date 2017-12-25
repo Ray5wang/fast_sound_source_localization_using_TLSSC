cd5becfe20e38f4b117de9eeb7503b4ebae487bd
labrick <yananback@gmail.com>
2017-12-25 19:46:00 +0800

    修改TDOA的搜索范围，后续需要继续调整，需要均匀，可以的话改成立体方格搜索，但需要考虑方格搜索的最远距离不够均匀问题

--- 3_gen_TLSSC_TDOA_table/main.m
+++ 3_gen_TLSSC_TDOA_table/main.m
@@ -36,18 +36,22 @@ mic=[0, 122.5, 438; ...		%0
 	-86.6, -86.6, 200; ...	%13 
 	-122.5, 0, 200; ...		%14
 	-86.6, 86.6, 200];  	%15
-% 不是最开始的[400, 400, 60]?
-mic_array_origin= [0 0 400];    % in the room
+% 不是最开始的[400, 400, 60]?，这里是建立搜索中心
+% mic_array_origin= [0 0 400];    % in the room
+mic_array_origin= [0 0 319];    % in the room
 
 % Search range
-search_range.theta= 0:1:359;        % 水平degree
-search_range.phi= 1:1:90;           % 垂直degree
-search_range.r= [2000 2500 3000];   % milli meter
+search_range.theta= 0:2:359;        % 水平degree
+% search_range.phi= 1:1:90;           % 垂直degree
+search_range.phi= 1:15:90;           % 垂直degree
+search_range.r= [800 1600 2300 2900 3400 3800];   % milli meter
+
+% fprint('search point = %d')
 
 Fs= 16000; % sampling frequency (Hz)
 c= 340000; % speed of sound (mm/s)
 
 mk_tdoa_table_full(mic,mic_array_origin,search_range,Fs,c);
-mk_tdoa_table_inverse('TDOA_table.mat');
-mk_tdoa_table_ssc('TDOA_table.mat','cartCoords.mat',mic,Fs,c);
-mk_tdoa_table_tlssc('TDOA_table.mat','TDOA_table_SSC.mat','SSC_centroids.mat',mic,Fs,c);
+% mk_tdoa_table_inverse('TDOA_table.mat');
+% mk_tdoa_table_ssc('TDOA_table.mat','cartCoords.mat',mic,Fs,c);
+% mk_tdoa_table_tlssc('TDOA_table.mat','TDOA_table_SSC.mat','SSC_centroids.mat',mic,Fs,c);
